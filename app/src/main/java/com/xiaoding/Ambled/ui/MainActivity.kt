fb1281f5c792877d8ededb98d6a88dc969e5837f
kabyding <1282324479@qq.com>
2019-07-16 20:16:59 +0800

    修复NavigationMenuItemView error

--- app/src/main/java/com/xiaoding/Ambled/ui/MainActivity.kt
+++ app/src/main/java/com/xiaoding/Ambled/ui/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.xiaoding.Ambled.ui
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import com.xiaoding.Ambled.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity:BaseActivity(){
+class MainActivity : AppCompatActivity() {
+    var navigationView: NavigationView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationView=findViewById<NavigationView>(R.id.nav_view)
+        navigationView!!.setItemIconTintList(null)//让侧滑菜单项显示本身颜色
+
+        setSupportActionBar(toolbar)
+        /*显示Home图标*/
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val mToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, 0, 0)
+        drawer_layout.addDrawerListener(mToggle)
+        /*同步drawerlayout的状态*/
+        mToggle.syncState()
+
+
+
     }
+
+
 }
